3c5e5b3cf93fbea3d79d0ca2956eee4fc721ca01
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-05-19 18:36:14 -0500

    Update auto bat farm.py

--- auto bat farm.py
+++ auto bat farm.py
@@ -1,8 +1,8 @@
 import pyautogui, keyboard, time
 
 print('██████╗░░█████╗░████████╗  ███████╗░█████╗░██████╗░███╗░░░███╗\n██╔══██╗██╔══██╗╚══██╔══╝  ██╔════╝██╔══██╗██╔══██╗████╗░████║\n██████╦╝███████║░░░██║░░░  █████╗░░███████║██████╔╝██╔████╔██║\n██╔══██╗██╔══██║░░░██║░░░  ██╔══╝░░██╔══██║██╔══██╗██║╚██╔╝██║\n██████╦╝██║░░██║░░░██║░░░  ██║░░░░░██║░░██║██║░░██║██║░╚═╝░██║\n╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░  ╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝');input("Press Enter to start...")
-if not input=='exit'or'quit'or'x':
- print('Started...')
- while True:
-  pyautogui.click(85,50);time.sleep(10)
-  except KeyboardInterrupt:break
+if not input=='x':
+  print('Started...')
+  while True:
+   pyautogui.click(85,50);time.sleep(5)
+   if keyboard.is_pressed('x'):break
